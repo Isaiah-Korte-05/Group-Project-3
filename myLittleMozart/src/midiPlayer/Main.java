1f8c0bf574b424f9dd1ecf7bf4537c69c8653a64
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-03-30 15:34:12 -0500

    added input security

--- myLittleMozart/src/midiPlayer/Main.java
+++ myLittleMozart/src/midiPlayer/Main.java
@@ -16,72 +16,97 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./files/mystery_song.csv");
+			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("C:\\Users\\isaia\\git\\Group-Project-3\\myLittleMozart\\src\\midiPlayer\\files\\mystery_song.csv");
 			Sequence sequence = new Sequence(Sequence.PPQ, 384);
 			Track track = sequence.createTrack();
+			
+			boolean doBreak = false;
 			
 			try (Scanner scanner = new Scanner(System.in)) {
 				MidiEventFactoryAbstract factoryAbstract = null;
 				MidiEventFactory factory = null;
 				
-				System.out.println("Please select which Note Length Behavior you wish to apply:");
-				System.out.println("[0] Standard Length");
-				System.out.println("[1] Legato");
-				System.out.println("[2] Staccato");
-				int eventSelector = scanner.nextInt();
-				
-				switch(eventSelector) {
-				case 0:
-					factoryAbstract = new StandardMidiEventFactoryAbstract();
-					factory = factoryAbstract.createFactory();
-					break;
-				case 1:
-					factoryAbstract = new LegatoMidiEventFactoryAbstract();
-					factory = factoryAbstract.createFactory();
-					break;
-				case 2:
-					factoryAbstract = new StaccatoMidiEventFactoryAbstract();
-					factory = factoryAbstract.createFactory();
-					break;
+				while(!doBreak) {
+					System.out.println("Please select which Note Length Behavior you wish to apply:");
+					System.out.println("[0] Standard Length");
+					System.out.println("[1] Legato");
+					System.out.println("[2] Staccato");
+					int eventSelector = scanner.nextInt();
+					
+					switch(eventSelector) {
+					case 0:
+						factoryAbstract = new StandardMidiEventFactoryAbstract();
+						factory = factoryAbstract.createFactory();
+						doBreak = true;
+						break;
+					case 1:
+						factoryAbstract = new LegatoMidiEventFactoryAbstract();
+						factory = factoryAbstract.createFactory();
+						doBreak = true;
+						break;
+					case 2:
+						factoryAbstract = new StaccatoMidiEventFactoryAbstract();
+						factory = factoryAbstract.createFactory();
+						doBreak = true;
+						break;
+					default:
+						System.err.println("Invalid input, please try again.");
+						break;
+					}
 				}
 				
 				InstrumentStrategy instrumentStrategy = null;
+				doBreak = false;
 				
-				System.out.println("Please select which instrument you wish to apply:");
-				System.out.println("[0] Acoustic Grand Piano");
-				System.out.println("[1] Electric Bass Guitar");
-				System.out.println("[2] Trumpet");
-				int instrumentSelector = scanner.nextInt();
-				
-				switch(instrumentSelector) {
-				case 0:
-					instrumentStrategy = new AcousticGrandPianoStrategy();
-					instrumentStrategy.applyInstrument(track, 0);
-					break;
-				case 1:
-					instrumentStrategy = new ElectricBassGuitarStrategy();
-					instrumentStrategy.applyInstrument(track, 0);
-					break;
-				case 2:
-					instrumentStrategy = new TrumpetStrategy();
-					instrumentStrategy.applyInstrument(track, 0);
-					break;
+				while(!doBreak) {
+					System.out.println("Please select which instrument you wish to apply:");
+					System.out.println("[0] Acoustic Grand Piano");
+					System.out.println("[1] Electric Bass Guitar");
+					System.out.println("[2] Trumpet");
+					int instrumentSelector = scanner.nextInt();
+					
+					switch(instrumentSelector) {
+					case 0:
+						instrumentStrategy = new AcousticGrandPianoStrategy();
+						instrumentStrategy.applyInstrument(track, 0);
+						doBreak = true;
+						break;
+					case 1:
+						instrumentStrategy = new ElectricBassGuitarStrategy();
+						instrumentStrategy.applyInstrument(track, 0);
+						doBreak = true;
+						break;
+					case 2:
+						instrumentStrategy = new TrumpetStrategy();
+						instrumentStrategy.applyInstrument(track, 0);
+						doBreak = true;
+						break;
+					default:
+						System.err.println("Invalid input, please try again.");
+					}
 				}
 				
 				PitchStrategy pitchStrategy = null;
+				doBreak = false;
 				
-				System.out.println("Please select which pitch adjustment you wish to apply:");
-				System.out.println("[0] Lower");
-				System.out.println("[1] Higher");
-				int pitchSelector = scanner.nextInt();
-				
-				switch(pitchSelector) {
-				case 0:
-					pitchStrategy = new LowerPitchStrategy();
-					break;
-				case 1:
-					pitchStrategy = new HigherPitchStrategy();
-					break;
+				while(!doBreak) {
+					System.out.println("Please select which pitch adjustment you wish to apply:");
+					System.out.println("[0] Lower");
+					System.out.println("[1] Higher");
+					int pitchSelector = scanner.nextInt();
+					
+					switch(pitchSelector) {
+					case 0:
+						pitchStrategy = new LowerPitchStrategy();
+						doBreak = true;
+						break;
+					case 1:
+						pitchStrategy = new HigherPitchStrategy();
+						doBreak = true;
+						break;
+					default:
+						System.err.println("Invalid input, please try again.");
+					}
 				}
 				
 				for(MidiEventData event : midiEvents) {
