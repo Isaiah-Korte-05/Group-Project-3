068adfcb0a38298f61c50b6276b944c5ee5e90eb
anna <150392971+annamori900@users.noreply.github.com>
2025-03-31 10:14:53 -0500

    add javadoc to implementing abstract factory classes

--- myLittleMozart/src/midiComposition/eventFactories/abstractFactories/LegatoMidiEventFactoryAbstract.java
+++ myLittleMozart/src/midiComposition/eventFactories/abstractFactories/LegatoMidiEventFactoryAbstract.java
@@ -6,6 +6,12 @@ import midiComposition.eventFactories.factoryTypes.LegatoMidiEventFactory;
 
 public class LegatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract {
 
+	/**
+	 * This creates a new instance of a factory with legato note length behavior,
+	 * or smooth and connected notes with longer durations of notes and minimal
+	 * gaps between notes.
+	 * @return a new LegatoMidiEventFactory instance.
+	 */
 	@Override
 	public MidiEventFactory createFactory() {
 		return new LegatoMidiEventFactory();
